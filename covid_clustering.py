215398737a6c10a5384618dd226b6505641f2524
D. Grin <dymong@yandex.ru>
2020-07-31 15:35:47 +0300

    Initial commit

---
+++ covid_clustering.py
@@ -0,0 +1,59 @@
+import hdbscan
+import numpy as np
+
+from math import sqrt
+
+from kneed import KneeLocator
+from sklearn.cluster import DBSCAN, MiniBatchKMeans
+from sklearn.neighbors import NearestNeighbors
+from sklearn.metrics.pairwise import pairwise_distances
+
+
+def get_cluster_labels(embeddings: np.ndarray, method: str) -> np.ndarray:
+    try:
+        methods = {
+            'dbscan': _get_cluster_labels_dbscan,
+            'hdbscan': _get_cluster_labels_hdbscan,
+            'kmeans': _get_cluster_labels_kmeans
+        }
+        function = methods[method]
+    except KeyError as key:
+        raise KeyError(f"Invalid method name: {key}! Available methods are: {tuple(methods.keys())}")
+    return function(embeddings)
+
+
+def find_epsilon(embeddings: np.ndarray) -> float:
+    k = round(sqrt(len(embeddings)))
+    nbrs = NearestNeighbors(n_neighbors=k, n_jobs=-1, algorithm='auto').fit(embeddings)
+    distances, _ = nbrs.kneighbors(embeddings)
+    distances = [np.mean(d) for d in np.sort(distances, axis=0)]
+    kneedle = KneeLocator(distances, list(range(len(distances))), online=True)
+    epsilon = np.mean(list(kneedle.all_elbows))
+    if epsilon == 0.0:
+        epsilon = np.mean(distances)
+    return float(epsilon)
+
+
+def _get_cluster_labels_hdbscan(embeddings: np.ndarray) -> np.ndarray:
+    distance_matrix = pairwise_distances(embeddings, n_jobs=-1, metric='cosine')
+    distance_matrix = np.array(distance_matrix, dtype='double')
+    clusterer = hdbscan.HDBSCAN(min_cluster_size=2,
+                                min_samples=2,
+                                metric='precomputed')
+    clusterer.fit(distance_matrix)
+    return clusterer.labels_
+
+
+def _get_cluster_labels_dbscan(embeddings: np.ndarray) -> np.ndarray:
+    epsilon = find_epsilon(embeddings)
+    cluster_labels = DBSCAN(eps=epsilon,
+                            min_samples=1,
+                            n_jobs=1) \
+        .fit_predict(embeddings)
+    return cluster_labels
+
+
+def _get_cluster_labels_kmeans(embeddings: np.ndarray) -> np.ndarray:
+    model = MiniBatchKMeans(n_clusters=5)
+    cluster_labels = model.fit_predict(embeddings)
+    return cluster_labels
---
+++ main.py
@@ -0,0 +1,94 @@
+import os
+import numpy as np
+import pandas as pd
+
+from typing import List, Dict
+from collections import Counter
+
+from summarizer import Summarizer
+from rake_nltk import Rake, Metric
+from sklearn.decomposition import PCA
+from sentence_transformers import SentenceTransformer
+from jinja2 import Environment, FileSystemLoader
+from transformers import AutoModel, AutoTokenizer, AutoConfig
+
+from covid_clustering import get_cluster_labels
+
+
+def main():
+    article_data = open_csv_file("../datasets/covid_articles/covid19_articles.csv")
+    texts = list(article_data["content"].values)
+    texts = get_summary(texts)
+
+    embeddings = encode_texts(texts)
+    embeddings = dimensionality_reduction(embeddings)
+
+    cluster_labels = get_cluster_labels(embeddings, method='hdbscan')
+    print(Counter(cluster_labels))
+    article_data["cluster_label"] = cluster_labels
+    article_data_clustered = article_data.groupby("cluster_label").apply(dataframe_regroup)
+
+    generate_html_report(article_data_clustered, "report.html")
+
+
+def open_csv_file(path: str) -> pd.DataFrame:
+    dataframe = pd.read_csv(path)
+    dataframe = dataframe.sample(n=100)
+    return dataframe
+
+
+def get_summary(texts: List[str]) -> List[str]:
+    config = AutoConfig.from_pretrained('allenai/biomed_roberta_base')
+    config.output_hidden_states = True
+    tokenizer = AutoTokenizer.from_pretrained('allenai/biomed_roberta_base')
+    model = AutoModel.from_pretrained('allenai/biomed_roberta_base', config=config)
+    summarizer = Summarizer(custom_model=model, custom_tokenizer=tokenizer)
+    result = [summarizer(text) for text in texts]
+    return result
+
+
+def encode_texts(texts: List[str]) -> List[np.ndarray]:
+    model = SentenceTransformer('roberta-base-nli-stsb-mean-tokens')
+    sentence_embeddings = model.encode(texts)
+    return sentence_embeddings
+
+
+def dimensionality_reduction(embeddings: List[np.ndarray]) -> np.ndarray:
+    pca = PCA()
+    result = pca.fit_transform(embeddings)
+    print(f"Dimensions: {len(embeddings)}x{len(embeddings[0])} -> {result.shape[0]}x{result.shape[1]}")
+    return pca.fit_transform(embeddings)
+
+
+def dataframe_regroup(dataframe: pd.DataFrame) -> Dict:
+    # text = '.\n'.join(dataframe["content"])
+    titles = dataframe['title'].values
+    size = dataframe.shape[0]
+    # common_phrases = extract_common_phrases(text)[:10]
+
+    return pd.Series({'titles': titles,
+                      'cluster_size': size})
+    #   'common_phrases': common_phrases})
+
+
+def extract_common_phrases(text: str) -> List[str]:
+    r = Rake(min_length=2, max_length=5, ranking_metric=Metric.WORD_FREQUENCY)
+    r.extract_keywords_from_text(text)
+    return r.get_ranked_phrases()
+
+
+def generate_html_report(dataframe: pd.DataFrame, output_file: str) -> None:
+    env = Environment(loader=FileSystemLoader(os.path.join(os.path.dirname(__file__))))
+    template = env.get_template("template.html")
+    original_list = dataframe.to_dict('records')
+    new_list = [{key: values for key, values in record.items()
+                 if key in ['cluster_size', 'titles']}
+                for record in original_list]
+    template_vars = {"values": new_list}
+    html_out = template.render(template_vars)
+    with open(output_file, "w", encoding="utf-8") as f:
+        f.write(html_out)
+
+
+if __name__ == "__main__":
+    main()
