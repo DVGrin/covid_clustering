9ecfc787e4456581334adea581eb40d92a40dd02
D. Grin <dymong@yandex.ru>
2020-10-09 12:13:26 +0300

    Added verbosity option, slightly improved structure

--- covid_encoding.py
+++ covid_encoding.py
@@ -5,35 +5,29 @@ from sentence_transformers import SentenceTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-def encode_texts(texts: List[str], method: str) -> np.ndarray:
+def encode_texts(texts: List[str], method: str, *, verbose: bool = False) -> np.ndarray:
     try:
         methods = {
             'roberta': _encode_texts_roberta,
-            'tf-idf': _encode_texts_tf_idf
+            'tf-idf': _encode_texts_tf_idf, 'tfidf': _encode_texts_tf_idf,
         }
         function = methods[method.lower()]
     except KeyError as key:
         raise KeyError(f"Invalid encoding method name: {key}! Available methods are: {tuple(methods.keys())}")
-    return function(texts)
+    return function(texts, verbose)
 
 
-def _encode_texts_roberta(texts: List[str]) -> np.ndarray:
+def _encode_texts_roberta(texts: List[str], verbose: bool = False) -> np.ndarray:
     model = SentenceTransformer('roberta-base-nli-stsb-mean-tokens')
-    sentence_embeddings = model.encode(texts, convert_to_numpy=True, show_progress_bar=True)
+    if verbose:
+        print("Started text encoding:")
+    sentence_embeddings = model.encode(texts, convert_to_numpy=True, show_progress_bar=verbose)
     return sentence_embeddings
 
 
-def _encode_texts_tf_idf(texts: List[str]) -> np.ndarray:
+def _encode_texts_tf_idf(texts: List[str], verbose: bool = False) -> np.ndarray:
     vectorizer = TfidfVectorizer(max_features=4096)
     result = vectorizer.fit_transform(texts)
-    return result
-
-
-def _preprocess_texts(texts: List[str]) -> List[str]:
-    pass
-
-
-def vectorize(text, maxx_features):
-    vectorizer = TfidfVectorizer(max_features=maxx_features)
-    X = vectorizer.fit_transform(text)
-    return X
+    if verbose:
+        print("Text encoding finished")
+    return result.toarray()
--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import os
 import numpy as np
 import pandas as pd
 
-from typing import List, Dict
-from collections import Counter
+from typing import List, Dict, Union
 
+from tqdm import tqdm
 from summarizer import Summarizer
 from rake_nltk import Rake, Metric
 from sklearn.decomposition import PCA
@@ -15,40 +15,58 @@ from covid_encoding import encode_texts
 from covid_clustering import get_cluster_labels
 
 
-def main():
-    article_data = open_csv_file("../datasets/covid_articles/covid19_articles.csv")
-    texts = list(article_data["content"].values)
-    texts = get_summary(texts)
+# Parameters
+VERBOSE = True
+USE_SUMMARY = False
+N_SAMPLES = 1000  # int or None
 
-    embeddings = encode_texts(texts, 'roberta')
+
+def main():
+    article_data = open_csv_file("../datasets/covid_articles/covid19_articles.csv", n_samples=N_SAMPLES, verbose=VERBOSE)
+    texts = list(article_data["content"].to_numpy())
+    if USE_SUMMARY:
+        texts = get_summary(texts, verbose=VERBOSE)
+
+    embeddings = encode_texts(texts, 'roberta', verbose=VERBOSE)
     embeddings = dimensionality_reduction(embeddings)
 
     cluster_labels = get_cluster_labels(embeddings, method='hdbscan')
-    print(Counter(cluster_labels))
+    if VERBOSE:
+        print("Clustering finished")
 
     article_data["cluster_label"] = cluster_labels
     article_data_clustered = article_data.groupby("cluster_label").apply(dataframe_regroup)
     generate_html_report(article_data_clustered, "report.html")
 
 
-def open_csv_file(path: str) -> pd.DataFrame:
+def open_csv_file(path: str, *, n_samples: Union[None, int] = None, verbose: bool = False) -> pd.DataFrame:
     dataframe = pd.read_csv(path)
-    dataframe = dataframe.sample(n=100)
+    if n_samples:
+        dataframe = dataframe.sample(n=n_samples)
+    if verbose:
+        if n_samples:
+            print(f"Chosen {n_samples} random samples from articles dataset")
+        else:
+            print(f"Articles dataset opened. Length: {dataframe.shape[0]}")
     return dataframe
 
 
-def get_summary(texts: List[str]) -> List[str]:
+def get_summary(texts: List[str], *, verbose: bool = False) -> List[str]:
     config = AutoConfig.from_pretrained('allenai/biomed_roberta_base')
     config.output_hidden_states = True
     tokenizer = AutoTokenizer.from_pretrained('allenai/biomed_roberta_base')
     model = AutoModel.from_pretrained('allenai/biomed_roberta_base', config=config)
     summarizer = Summarizer(custom_model=model, custom_tokenizer=tokenizer)
+    if verbose:
+        print("Summary extraction started:")
+        texts = tqdm(texts)
     result = [summarizer(text) for text in texts]
     return result
 
 
 def dimensionality_reduction(embeddings: np.ndarray) -> np.ndarray:
-    pca = PCA()
+    max_pca_components = 768
+    pca = PCA(n_components=(min(N_SAMPLES or max_pca_components, max_pca_components)))
     result = pca.fit_transform(embeddings)
     print(f"Dimensions: {len(embeddings)}x{len(embeddings[0])} -> {result.shape[0]}x{result.shape[1]}")
     return pca.fit_transform(embeddings)
